0b7f35d34f3fe4982734a54a3b60709be84a6162
Sam Colville <scolville47@gmail.com>
2018-06-17 01:10:16 +0100

    loop functionality added

--- TimeCalculator/Program.cs
+++ TimeCalculator/Program.cs
@@ -8,31 +8,41 @@ namespace TimeCalculator
         {
             //Variables
             double payRate = 0, costs = 0, costInHours = 0;
-            //string payRateStr, daysStr, timeInDaysStr;
+            string payRateStr = "";
 
             Console.WriteLine("Welcome to the time calculator.");
             Console.WriteLine("Find out what that dress or those rollies cost in the time you work!");
             Console.WriteLine("Type 'exit' to quit at any point.");
             Console.WriteLine("First of all, we need to know how much you earn per hour.");
-            Console.WriteLine();
-            payRate = GetPayRate();
-            Console.WriteLine();
-            Console.WriteLine("Next, enter how much the item(s) cost.");
+            payRateStr = GetPayRate();
 
-            costs = GetCost();
+            while(payRateStr != "exit")
+            {
+                payRate = double.Parse(payRateStr);
+                Console.WriteLine();
+                Console.WriteLine("Next, enter how much the item(s) cost.");
+                costs = GetCost();
 
-            costInHours = costs / payRate;
+                costInHours = costs / payRate;
 
-            Console.WriteLine("That is worth {0:0.0}hrs", costInHours);
+                Console.WriteLine("That is worth {0:0.0}hrs", costInHours);
+
+                Console.WriteLine("\n\n\n");
+                payRateStr = GetPayRate();
+
+            }
+
+            Console.WriteLine("Thank you for using this program");
 
         }
 
-        static double GetPayRate()
+        static string GetPayRate()
         {
-            double payRate;
+            string payRate = "";
 
-            Console.Write("Enter your hourly rate:  £");
-            payRate = double.Parse(Console.ReadLine());
+            Console.Write("Enter your hourly rate (exit to quit):  £");
+            payRate = Console.ReadLine();
+            payRate = payRate.ToLower();
 
             return payRate;
         }
